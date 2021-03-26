3af52888fa2b6352aee72353af406995367a244f
addempsea <babatundeademola4@gmail.com>
2021-03-26 13:38:25 +0100

    add loader to add wallet

--- components/AddWallet.jsx
+++ components/AddWallet.jsx
@@ -1,22 +1,31 @@
+import { useState } from "react";
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import { createWallet } from "../queries";
 import { toast } from "react-toastify";
 import { useSession } from "next-auth/client";
+import { Shadows } from "./Shadows";
 const AddWallet = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [session] = useSession();
   const handleAdd = async () => {
     try {
+      setIsLoading(true);
       const { data } = await createWallet(session?.user?.token);
-      data.status === 201 ? toast.success("Wallet successfully created") : new Error();
+      data.status === 201
+        ? toast.success("Wallet successfully created")
+        : new Error();
 
       window.location.reload();
     } catch (error) {
+      setIsLoading(false);
       toast.error("Something went wrong");
     }
   };
+
   return (
     <Flex
       w="100%"
+      pos="relative"
       boxShadow="0px 0px 15px rgba(163, 175, 191, 0.15)"
       borderRadius="2px"
       align="center"
@@ -25,7 +34,29 @@ const AddWallet = () => {
       cursor="pointer"
       onClick={handleAdd}
     >
-      <Flex boxSize="41px" borderRadius="50%" bg="brand.dark" align="center" justify="center">
+      {isLoading && (
+        <Flex
+          top="0"
+          left="0"
+          pos="absolute"
+          w="100%"
+          px="30px"
+          h="88px"
+          alignItems="center"
+          cursor="not-allowed"
+          bg="white"
+          opacity="0.8"
+        >
+          <Box className="spinner"></Box>
+        </Flex>
+      )}
+      <Flex
+        boxSize="41px"
+        borderRadius="50%"
+        bg="brand.dark"
+        align="center"
+        justify="center"
+      >
         <Image src="/icons/plus.svg" w="15px" />
       </Flex>
       <Box ml="16px">
